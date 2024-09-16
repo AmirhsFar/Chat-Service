e98f52b035d0ee018757000b5cf809601fff9a5f
AmirhsFar <amirhosseinfarahani75@yahoo.com>
2024-09-16 23:26:16 +0330

    socket.io connected successfully

---
+++ FastAPI-Learning/socketio/server/sockets.py
@@ -0,0 +1,26 @@
+from socketio import (
+    AsyncServer, ASGIApp
+)
+
+sio_server = AsyncServer(
+    async_mode='asgi',
+    cors_allowed_origins=[]
+)
+
+sio_app = ASGIApp(
+    socketio_server=sio_server,
+    socketio_path='sockets'
+)
+
+@sio_server.event
+async def connect(sid, _, __):
+    print(f'{sid}: is connected')
+    await sio_server.emit('join', {'sid': sid})
+
+@sio_server.event
+async def chat(sid, message):
+    await sio_server.emit('chat', {'sid': sid, 'message': message})
+
+@sio_server.event
+async def disconnect(sid):
+    print(f'{sid}: is disconnected')
